9b943bec4e6479d16a9a0da9bae27aaa71bd5b71
stoftot <gryn@itu.dk>
2025-09-18 18:02:49 +0200

    Updated loaders for traning til i have better soloution

--- RealJapanese/Repositories/GreetingsData.cs
+++ RealJapanese/Repositories/GreetingsData.cs
@@ -1,7 +1,33 @@
+using DataLoaders;
+using Repositories.DTOs;
+
 namespace Repositories;
 
-public class GreetingsData() : QuestionAnswerBase(FolderPath, FileName)
+public class GreetingsData
 {
     private static string FolderPath => "../Data";
-    private static string FileName => "Greetings.csv";
+    private static string FileNameGreetings => "Greetings.csv";
+    private static string FileNameExpressions => "Expressions.csv";
+    private static string FileNamePerson => "Person.csv";
+    private static string FileNameSchool => "School.csv";
+    private static string FileNameOther => "Other.csv";
+    
+    private readonly List<QuestionAnswerDto> _questionAnswers;
+        
+    public List<QuestionAnswerDto> QuestionAnswers => _questionAnswers.ToList();
+
+    public GreetingsData()
+    {
+        var greetingsLoader = new QuestionAnswerLoader(FolderPath, FileNameGreetings);
+        var expressionsLoader = new QuestionAnswerLoader(FolderPath, FileNameExpressions);
+        var personLoader = new QuestionAnswerLoader(FolderPath, FileNamePerson);
+        var schoolLoader = new QuestionAnswerLoader(FolderPath, FileNameSchool);
+        var otherLoader = new QuestionAnswerLoader(FolderPath, FileNameOther);
+        _questionAnswers = [];
+        _questionAnswers.AddRange(greetingsLoader.Elements.Select(QuestionAnswerDto.FromModel));
+        _questionAnswers.AddRange(expressionsLoader.Elements.Select(QuestionAnswerDto.FromModel));
+        _questionAnswers.AddRange(personLoader.Elements.Select(QuestionAnswerDto.FromModel));
+        _questionAnswers.AddRange(schoolLoader.Elements.Select(QuestionAnswerDto.FromModel));
+        _questionAnswers.AddRange(otherLoader.Elements.Select(QuestionAnswerDto.FromModel));
+    }
 }
--- RealJapanese/Repositories/NumbersData.cs
+++ RealJapanese/Repositories/NumbersData.cs
@@ -3,8 +3,26 @@ using Repositories.DTOs;
 
 namespace Repositories;
 
-public class NumbersData() : QuestionAnswerBase(FolderPath, FileName)
+public class NumbersData
 {
     private static string FolderPath => "../Data";
     private static string FileName => "Numbers.csv";
+    private static string FileNameTime => "NumbersTime.csv";
+    
+    private static string FileNameage => "NumbersAge.csv";
+    
+    private readonly List<QuestionAnswerDto> _questionAnswers;
+        
+    public List<QuestionAnswerDto> QuestionAnswers => _questionAnswers.ToList();
+
+    public NumbersData()
+    {
+        var numbersLoader = new QuestionAnswerLoader(FolderPath, FileName);
+        var timeLoader = new QuestionAnswerLoader(FolderPath, FileNameTime);
+        var ageLoader = new QuestionAnswerLoader(FolderPath, FileNameage);
+        _questionAnswers = [];
+        // _questionAnswers.AddRange(numbersLoader.Elements.Select(QuestionAnswerDto.FromModel));
+        _questionAnswers.AddRange(timeLoader.Elements.Select(QuestionAnswerDto.FromModel));
+        _questionAnswers.AddRange(ageLoader.Elements.Select(QuestionAnswerDto.FromModel));
+    }
 }
