c19e475699c5e650fbf3a28ed9dc6eb739db3c25
stoftot <gryn@itu.dk>
2025-11-11 23:30:36 +0100

    Created loaders for json sentences

---
+++ RealJapanese/DataLoaders/SentenceLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using DataLoaders.Models;
+
+namespace DataLoaders;
+
+public class SentenceLoader(string filename)
+{
+    private static string FolderPath => "../Data/Sentences";
+    private string FileName { get; } = filename;
+    private string FilePath => $"{FolderPath}/{FileName}";
+
+
+    public List<Sentence> Load()
+    {
+        var fileType = FileName.Split('.').Last();
+
+        return fileType switch
+        {
+            "jsonl" => LoadJsonl(),
+            "json" => LoadJson(),
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    private List<Sentence> LoadJsonl()
+    {
+        return (from line
+                    in File.ReadLines(FilePath)
+                where !string.IsNullOrWhiteSpace(line)
+                select JsonSerializer.Deserialize<Sentence>(line))
+            .ToList();
+    }
+
+    private List<Sentence> LoadJson()
+    {
+        var json = File.ReadAllText(FilePath);
+        return JsonSerializer.Deserialize<List<Sentence>>(json) ?? [];
+    }
+}
