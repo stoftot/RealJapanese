c216ca0c3ffb5a05ce3322566883ff54d82e2444
stoftot <gryn@itu.dk>
2025-11-11 23:55:05 +0100

    Create TrainingNonsensSentences.cs

---
+++ RealJapanese/Repositories/TrainingNonsensSentences.cs
@@ -0,0 +1,13 @@
+﻿using DataLoaders.Models;
+
+namespace Repositories;
+
+public class TrainingNonsensSentences
+{
+    public List<Sentence> Sentences { get; }
+    public TrainingNonsensSentences()
+    {
+        var loader = new DataLoaders.SentenceLoader("jp_kana_reading_dataset_v3.jsonl");
+        Sentences = loader.Load();
+    }
+}
