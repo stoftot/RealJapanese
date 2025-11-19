a49774fb2435275070039be76088ee826e48709b
stoftot <gryn@itu.dk>
2025-11-19 22:51:05 +0100

    (add) pratice selector component
    A component to easily display different pratice methods for a topic

---
+++ RealJapanese/RealJapanese/Components/Shared/PraticeSelector.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RealJapanese.Components.Shared;
+
+public partial class PraticeSelectorBase : ComponentBase
+{
+    [Parameter] public string MainUrl { get; set; } = "";
+    [Parameter] public List<PageDescreption> Pages { get; set; } = [];
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+
+    protected void GoToUrl(PageDescreption page)
+    {
+        NavigationManager.NavigateTo($"{MainUrl}/{page.Url}");
+    }
+    
+    public class PageDescreption
+    {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Url { get; set; } = "";
+    }
+}
