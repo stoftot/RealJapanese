0732a3eb3c85c8405678b45f4dd6fa55a818306e
stoftot <gryn@itu.dk>
2025-12-01 16:58:27 +0100

    (add) exstension for the word model

---
+++ RealJapanese/Repositories/Exstensions/WordExstensions.cs
@@ -0,0 +1,15 @@
+﻿using DataLoaders.Models;
+using Repositories.DTOs;
+using WanaKanaSharp;
+
+namespace DataLoaders.Exstensions;
+
+public static class WordExstensions
+{
+    public static IEnumerable<QuestionAnswerDto> EnglishToRomajiQuestions(this IEnumerable<Word> words) =>
+        words.Select(v => new QuestionAnswerDto
+        {
+            Answer = WanaKana.ToRomaji(v.Kana),
+            Question = v.English
+        });
+}
