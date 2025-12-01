d8de04290e1518c783179f93ce4da1b898d18001
stoftot <gryn@itu.dk>
2025-12-01 16:57:58 +0100

    (add) toggle slider component

---
+++ RealJapanese/RealJapanese/Components/Shared/ToggleSlider.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RealJapanese.Components.Shared;
+
+public  class ToggleSliderBase : ComponentBase
+{
+    [Parameter] public string LeftText { get; set; } = "Left";
+    [Parameter] public string RightText { get; set; } = "Right";
+
+    // The selected side: false = left, true = right
+    [Parameter] public bool Value { get; set; }
+
+    // For two-way binding: @bind-Value
+    [Parameter] public EventCallback<bool> ValueChanged { get; set; }
+
+    [Parameter] public string? CssClass { get; set; }
+
+    protected async Task Toggle()
+    {
+        Value = !Value;
+        await ValueChanged.InvokeAsync(Value);
+    }
+}
