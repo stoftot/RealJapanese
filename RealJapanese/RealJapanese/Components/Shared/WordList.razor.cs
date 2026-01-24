aa0cafe610caef1c2420c518de0e2195cd54d314
Christoffer grünberg <gryn@itu.dk>
2026-01-24 13:35:08 +0100

    (upd) word list to be generic

--- RealJapanese/RealJapanese/Components/Shared/WordList.razor.cs
+++ RealJapanese/RealJapanese/Components/Shared/WordList.razor.cs
@@ -3,30 +3,30 @@ using Microsoft.AspNetCore.Components;
 
 namespace RealJapanese.Components.Shared;
 
-public class WordListBase : ComponentBase
+public class WordListBase<TItem> : ComponentBase where TItem : Word
 {
     [Parameter] public string Title { get; set; } = string.Empty;
 
     // All words that *can* be shown/selected
-    [Parameter] public List<Word> AllWords { get; set; } = [];
+    [Parameter] public List<TItem> AllWords { get; set; } = [];
 
     // IDs of the words that are currently selected in this list
     [Parameter] public List<int> SelectedIds { get; set; } = [];
     
-    [Parameter] public EventCallback<Word> WordSelected { get; set; }
+    [Parameter] public EventCallback<TItem> WordSelected { get; set; }
     
-    [Parameter] public EventCallback<Word> WordDeselected { get; set; }
+    [Parameter] public EventCallback<TItem> WordDeselected { get; set; }
 
     // Selected words at the top, in the order of SelectedIds;
     // unselected words below, sorted by Japanese.
-    protected IEnumerable<Word> OrderedItems =>
+    protected IEnumerable<TItem> OrderedItems =>
         AllWords
             .OrderBy(w => IsSelected(w.Id) ? 0 : 1)
             .ThenBy(w => w.Id);
 
     private bool IsSelected(int id) => SelectedIds.Contains(id);
 
-    protected async Task OnItemClicked(Word item)
+    protected async Task OnItemClicked(TItem item)
     {
         var wasSelected = SelectedIds.Contains(item.Id);
 
