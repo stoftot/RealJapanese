7f309fc4882aca93a30cc8cfd9da49a76f39d5fe
Christoffer grünberg <gryn@itu.dk>
2026-01-27 15:40:40 +0100

    Create KanjiData.cs

---
+++ RealJapanese/Repositories/KanjiData.cs
@@ -0,0 +1,24 @@
+﻿using DataLoaders.Models;
+using Repositories.Bases;
+
+namespace Repositories;
+
+public class KanjiData
+{
+    private const string BaseFolderPath = "../Data/Kanji/";
+    
+    public class SingelData() : WordDataBase<Word>(FolderPath, DataFileName)
+    {
+        private const string FolderPath = BaseFolderPath + "Singel/";
+        private const string DataFileName = "Singel.json";
+    }
+    
+    public class CombinedData() : WordDataBase<Word>(FolderPath, DataFileName)
+    {
+        private const string FolderPath = BaseFolderPath + "Combined/";
+        private const string DataFileName = "Combined.json";
+    }
+    
+    public SingelData Singel { get; } = new();
+    public CombinedData Combined { get; } = new();
+}
