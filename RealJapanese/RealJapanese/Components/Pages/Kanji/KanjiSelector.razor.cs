3c57d2a2e3dd453fe523801fe78d9dbfcbe46b09
Christoffer grünberg <gryn@itu.dk>
2026-01-27 17:25:30 +0100

    (add) kanji selector

---
+++ RealJapanese/RealJapanese/Components/Pages/Kanji/KanjiSelector.razor.cs
@@ -0,0 +1,57 @@
+﻿using DataLoaders.Models;
+using Microsoft.AspNetCore.Components;
+using RealJapanese.Components.Shared;
+using Repositories;
+using Repositories.Bases;
+
+namespace RealJapanese.Components.Pages.Kanji;
+
+public class KanjiSelectorBase : WordComponentBase<Word>
+{
+    private bool singelKanjiMode = false;
+
+    protected bool SingelKanjiMode
+    {
+        get => singelKanjiMode;
+        set
+        {
+            if (singelKanjiMode == value) return;
+            singelKanjiMode = value;
+            OnInitialized();
+        }
+    }
+    
+    [Inject] private KanjiData KanjiDataInjected { get; set; } = null!;
+    protected override WordDataBase<Word> WordData => 
+        SingelKanjiMode ? KanjiDataInjected.Singel : KanjiDataInjected.Combined;
+
+    public List<PraticeSelectorBase.PageDescreption> GetPageDescreptions()
+    {
+        var pagesDescreptions = new List<PraticeSelectorBase.PageDescreption>();
+
+        if (SingelKanjiMode)
+        {
+            pagesDescreptions.Add(
+                new()
+                {
+                    Title = "Learn the kanji",
+                    Description = "Practice the meaning of the individual kanji",
+                    Url = $"meaning?training={Training}",
+                }
+            );
+        }
+        else
+        {
+            pagesDescreptions.Add(
+                new()
+                {
+                    Title = "Learn the kanji",
+                    Description = "Practice then meaning of the kanji in words",
+                    Url = $"meaning?training={Training}",
+                }
+            );
+        }
+        return pagesDescreptions;
+    }
+        
+}
