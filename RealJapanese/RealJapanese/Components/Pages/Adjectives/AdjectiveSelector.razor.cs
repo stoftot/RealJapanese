347a7dd835dc493b50e5335b4187d25924ae9240
Christoffer grünberg <gryn@itu.dk>
2026-02-01 14:10:18 +0100

    (add) selector for adjectives

---
+++ RealJapanese/RealJapanese/Components/Pages/Adjectives/AdjectiveSelector.razor.cs
@@ -0,0 +1,13 @@
+﻿using DataLoaders.Models;
+using Microsoft.AspNetCore.Components;
+using RealJapanese.Components.Shared;
+using Repositories;
+using Repositories.Bases;
+
+namespace RealJapanese.Components.Pages.Adjectives;
+
+public class AdjectiveSelectorBase : WordComponentBase<Adjective>
+{
+    [Inject] private AdjectiveData WordDataInjected { get; set; } = null!;
+    protected override WordDataBase<Adjective> WordData => WordDataInjected;
+}
