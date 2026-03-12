3de13c2d4bb85d6d1a6b8a9fe6c89b1d8ebe4916
Christoffer grünberg <gryn@itu.dk>
2026-03-12 17:39:19 +0100

    Update RomajiExstension.cs

--- RealJapanese/Repositories/Exstensions/RomajiExstension.cs
+++ RealJapanese/Repositories/Exstensions/RomajiExstension.cs
@@ -1,8 +1,16 @@
-﻿namespace Repositories.Exstensions;
+﻿using System.Text.RegularExpressions;
+
+namespace Repositories.Exstensions;
 
 public static class RomajiExstension
 {
     public static string ToRomaji(this string kana) => 
-        WanaKanaSharp.WanaKana.ToRomaji(kana).Replace("'", "").Replace("~", "");
+        Regex.Replace(
+            WanaKanaSharp.WanaKana
+                .ToRomaji(kana)
+                .Replace("'", "")
+                .Replace("~", ""),
+            "([aeiou])-", "$1$1"
+            );
 
 }
