5f9c0043d490b73c6b56be895c085efe12383336
Christoffer grünberg <gryn@itu.dk>
2026-04-09 16:57:49 +0200

    Added pretty to string for gojugationType

--- RealJapanese/DataLoaders/Models/Conjugatabel.cs
+++ RealJapanese/DataLoaders/Models/Conjugatabel.cs
@@ -24,3 +24,23 @@ public abstract record Conjugatabel : Word
 
     public abstract string Conjugate(ToConjugate toConjugate, ConjugationType conjugationType);
 }
+
+public static class ConjugationTypeExtensions
+{
+    public static string ToDisplayString(this Conjugatabel.ConjugationType conjugationType)
+    {
+        switch (conjugationType)
+        {
+            case Conjugatabel.ConjugationType.PresentAffirmative:
+                return "present affirmative";
+            case Conjugatabel.ConjugationType.PresentNegative:
+                return "present negative";
+            case Conjugatabel.ConjugationType.PastAffirmative:
+                return "past affirmative";
+            case Conjugatabel.ConjugationType.PastNegative:
+                return "past negative";
+            default:
+                throw new ArgumentOutOfRangeException(nameof(conjugationType), conjugationType, null);
+        }
+    }
+}
