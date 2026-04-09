ba5fc03e70a35fdbde07f62a79a46ab8cba00816
Christoffer grünberg <gryn@itu.dk>
2026-04-09 16:58:49 +0200

    Update VerbExstensions.cs

--- RealJapanese/Repositories/Exstensions/VerbExstensions.cs
+++ RealJapanese/Repositories/Exstensions/VerbExstensions.cs
@@ -8,6 +8,7 @@ public static class VerbExstensions
     public static IEnumerable<QuestionAnswerDto> EnglishToRomajiAndTypeQuestion(this IEnumerable<Verb> verbs)
         => verbs
             // .Where(v => v.Kana.ToRomaji()[^2..] == "ru" && v.TypeShortForm()!="ir")
+            // .Where(v => v.Kana.ToRomaji().Length >= 3 && (v.Kana.ToRomaji()[^3..] == "iru" || v.Kana.ToRomaji()[^3..] == "eru") && v.TypeShortForm()!="ir")
             .Select(v => new QuestionAnswerDto
         {
             Question = v.English,
