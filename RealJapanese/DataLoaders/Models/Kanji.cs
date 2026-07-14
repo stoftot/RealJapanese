3d8e9985f319a6751b78067daccc7c65a3bbec42
Christoffer grünberg <gryn@itu.dk>
2026-07-14 23:24:19 +0200

    Data

---
+++ RealJapanese/DataLoaders/Models/Kanji.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using DataLoaders.JsonConverters;
+
+namespace DataLoaders.Models;
+
+public record Kanji
+{
+    [JsonPropertyName("id")] 
+    [JsonConverter(typeof(StringToIntConverter))]
+    public int Id { get; set; } = -1;
+
+    [JsonPropertyName("symbol")]
+    public required string Symbol { get; init; }
+    
+    [JsonPropertyName("onReading")]
+    public required string OnReading { get; init; }
+
+    [JsonPropertyName("kunReading")]
+    public required string KunReading { get; init; }
+    
+    [JsonPropertyName("commonEnglish")]
+    public required string CommonEnglish { get; init; }
+    
+    [JsonPropertyName("otherEnglish")]
+    public required List<string> OtherEnglish { get; init; } = [];
+}
---
+++ RealJapanese/DataLoaders/Models/KanjiRelationData.cs
@@ -0,0 +1,9 @@
+﻿namespace DataLoaders.Models;
+
+public class KanjiRelationData
+{
+    public int KanjiId { get; set; }
+    public List<int> VerbIds { get; set; }
+    public List<int> AdjectiveIds { get; set; }
+    public List<int> WordIds { get; set; }
+}
